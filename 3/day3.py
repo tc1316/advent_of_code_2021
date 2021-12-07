c85c3d5548024bdb8c3172e60903298fbe535d0f
BoneSponge <50413143+BoneSponge@users.noreply.github.com>
2021-12-07 18:37:39 +0000

    Day3 1st star achieved

---
+++ 3/day3.py
@@ -0,0 +1,43 @@
+lines = []
+with open('input.txt') as f:
+    lines = f.readlines()
+
+n = len(lines[0]) #Length of one binary number
+
+gamma_bits = []
+epsilon_bits = []
+
+def gamma():
+  bits = []
+  for i in range(0,n-1):
+    tmp = { "zeroes": 0, "ones": 0}
+    for line in lines:
+      if int(line[i]) == 0:
+        tmp["zeroes"] += 1
+      elif int(line[i]) == 1:
+        tmp["ones"] += 1
+    if tmp["zeroes"] > tmp["ones"]:
+      bits.append(0)
+    else:
+      bits.append(1)
+  return int("".join(map(str, bits)),2)
+  
+def epsilon():
+  bits = []
+  for i in range(0,n-1):
+    tmp = { "zeroes": 0, "ones": 0}
+    for line in lines:
+      if int(line[i]) == 0:
+        tmp["zeroes"] += 1
+      elif int(line[i]) == 1:
+        tmp["ones"] += 1
+    if tmp["zeroes"] < tmp["ones"]:
+      bits.append(0)
+    else:
+      bits.append(1)
+  return int("".join(map(str, bits)),2)
+
+print(gamma()*epsilon())
+
+
+  
